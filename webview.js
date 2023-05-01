84e4de92a0817e7fad19c9c94a5752ea30c8dfb8
Nguyen The Tien <tokentien@gmail.com>
2023-05-02 06:49:23 +0700

    a

--- webview.js
+++ webview.js
@@ -1,22 +1,25 @@
-import path from 'path';
+// import path from 'path';
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
 
-    const elements = document.querySelectorAll('.bsU');
-    elements.forEach((el) => {
-      count += parseInt(el.textContent, 10);
-    });
-
-    if (document.getElementsByClassName('J-Ke n0').length > 0) {
-      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') != null) {
-        count = parseInt((document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') || '').replace(/[^0-9.]/g, ''), 10);
-      }
+    // const elements = document.querySelectorAll('.bsU');
+    // elements.forEach((el) => {
+    //   count += parseInt(el.textContent, 10);
+    // });
+    //
+    // if (document.getElementsByClassName('J-Ke n0').length > 0) {
+    //   if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') != null) {
+    //     count = parseInt((document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') || '').replace(/[^0-9.]/g, ''), 10);
+    //   }
+    // }
+    //
+    // const chatCount = parseInt(((document.querySelector('[data-tooltip="Chat"]') || '').ariaLabel || '').replace(/[^\d]/g, ''), 10) || 0;
+    let dom = document.querySelector(`.aio.UKr6le .bsU`);
+    if (dom) {
+      count = dom.textContent;
     }
-
-    const chatCount = parseInt(((document.querySelector('[data-tooltip="Chat"]') || '').ariaLabel || '').replace(/[^\d]/g, ''), 10) || 0;
-
 
     // Just incase we don't end up with a number, set it back to zero (parseInt can return NaN)
     count = parseInt(count, 10);
@@ -24,13 +27,13 @@ module.exports = (Franz) => {
       count = 0;
     }
 
-    count += chatCount;
+    // count += chatCount;
 
     // set Franz badge
     Franz.setBadge(count);
   };
 
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  // Franz.injectCSS(path.join(__dirname, 'service.css'));
   // check for new messages every second and update Franz badge
   Franz.loop(getMessages);
 };
